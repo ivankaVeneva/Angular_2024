c6342d5870f85bb800b5e4202b8888650fb2f403
DESKTOP-GDCQ9GM\user_h <veneva@imbm.bas.bg>
2024-11-29 19:28:05 +0200

    Subscribe for events

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { beaverImgSrc, delsysImgSrc } from './constants';
+import { TestDemoComponent } from "./test-demo/test-demo.component";
 
 
 @Component({
@@ -14,22 +14,11 @@ import { beaverImgSrc, delsysImgSrc } from './constants';
     NavigationBarComponent,
     FooterComponent,
     UserListComponent,
-  ],
+    TestDemoComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'demo';
-  numbers = [1, 2, 3, 4, 5];
-  
-  isShown = false;
-  randomMsg = `random-red`;
-  beaverImg = beaverImgSrc;
-  delsysImg = delsysImgSrc;
-
- togleModal(event: Event): void {
-  // console.log({ event});
-  this.isShown = !this.isShown;
-  this.randomMsg = this.isShown ? `random-green` : `random-red`;
- }
-}
+  }
