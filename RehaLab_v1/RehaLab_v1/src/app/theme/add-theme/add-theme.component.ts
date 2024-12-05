3895bc5d7fe510fb72b5c4a1397053d537b85250
DESKTOP-GDCQ9GM\user_h <veneva@imbm.bas.bg>
2024-12-05 22:45:59 +0200

    Register Form (Reactive). Login Form (Template-Driven). Profile Form (Reactive)

--- RehaLab_v1/RehaLab_v1/src/app/theme/add-theme/add-theme.component.ts
+++ RehaLab_v1/RehaLab_v1/src/app/theme/add-theme/add-theme.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-theme',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-theme.component.html',
   styleUrl: './add-theme.component.css'
 })
 export class AddThemeComponent {
   constructor(private apiservice: ApiService) {}
 
-  addTheme(event: Event, themeName: string, postText: string) {
-    event.preventDefault();
-    console.log({ themeName, postText });
-    this.apiservice.createTheme(themeName, postText).subscribe((data) => {
-      console.log(data);
-    });
+  addTheme(form: NgForm) {
+    console.log(form);
+    
+    if(form.invalid) {
+      return;
+    }
+    console.log(form.value);
+    
+  
+    // console.log({ themeName, postText });
+    // this.apiservice.createTheme(themeName, postText).subscribe((data) => {
+    //   console.log(data);
+    // });
   }
 }
