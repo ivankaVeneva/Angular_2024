a272f791894c0d7c3ed29ff49cea598cd8384216
DESKTOP-GDCQ9GM\user_h <veneva@imbm.bas.bg>
2024-12-11 23:13:10 +0200

    RehaLab_v1 and Rest-api have been copied to Ang_Project

---
+++ Ang_Project/RehaLab_v1/src/app/theme/themes-list/themes-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Theme } from '../../types/theme';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { RouterLink } from '@angular/router';
+import { SlicePipe } from '../../shared/pipes/slice.pipe';
+import { ElapsedTimePipe } from '../../shared/pipes/elapsed-time.pipe';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-themes-list',
+  standalone: true,
+  imports: [LoaderComponent, RouterLink, SlicePipe, DatePipe],
+  templateUrl: './themes-list.component.html',
+  styleUrl: './themes-list.component.css',
+})
+export class ThemesListComponent implements OnInit {
+  themes: Theme[] = [];
+  isLoading = true;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getThemes().subscribe((themes) => {
+      this.themes = themes;
+      this.isLoading = false;
+    });
+  }
+}
