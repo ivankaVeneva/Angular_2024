956f20c0402daa081027ed53066b9e1f432c0792
DESKTOP-GDCQ9GM\user_h <veneva@imbm.bas.bg>
2024-12-17 14:33:49 +0200

    Delete old versions

--- Ang_Project/RehaLab_v1/src/app/guards/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivateFn } from "@angular/router";
-import { UserService } from "../user/user.service";
-import { inject } from "@angular/core";
-
-export const AuthGuard: CanActivateFn = () => {
-    const userService =  inject(UserService);
-   // const router = inject(Router);
-    return userService.isLogged;
-};
